4350d2811226c4b7ee9664166acd6ed751d5385e
margo-chek <mnchek@mail.ru>
2023-05-12 00:07:19 +0300

    Поля классов сделаны приватными

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -11,64 +11,71 @@ import FormDestinationView from './../view/form-destination-view.js';
 import FormOffersView from './../view/form-offers-view.js';
 
 export default class MainPresenter {
-  contentComponent = new TripEventsListView();
-  formComponent = new EditFormView();
-  formDetailsComponent = new FormDetailsView();
+  #contentComponent = new TripEventsListView();
+  #formComponent = new EditFormView();
+  #formDetailsComponent = new FormDetailsView();
+
+  #parentContainer = null;
+  #pointsModel = null;
+  #offersModel = null;
+  #destinationsModel = null;
+
+  #tripPoints = [];
+  #destinations = [];
 
   constructor({parentContainer, pointsModel, offersModel, destinationsModel}) {
-    this.parentContainer = parentContainer;
-    this.pointsModel = pointsModel;
-    this.offersModel = offersModel;
-    this.destinationsModel = destinationsModel;
+    this.#parentContainer = parentContainer;
+    this.#pointsModel = pointsModel;
+    this.#offersModel = offersModel;
+    this.#destinationsModel = destinationsModel;
   }
 
   init() {
-    const tripMainElement = this.parentContainer.querySelector('.trip-main');
+    const tripMainElement = this.#parentContainer.querySelector('.trip-main');
     const tripControlsElement = tripMainElement.querySelector('.trip-controls__filters');
-    const tripEventsElement = this.parentContainer.querySelector('.trip-events');
-    const formElement = this.formComponent.element.querySelector('.event');
+    const tripEventsElement = this.#parentContainer.querySelector('.trip-events');
+    const formElement = this.#formComponent.element.querySelector('.event');
 
-    this.tripPoints = this.pointsModel.get();
-    this.offers = this.offersModel.get();
-    this.destinations = this.destinationsModel.get();
+    this.#tripPoints = this.#pointsModel.points;
+    this.#destinations = this.#destinationsModel.destinations;
 
-    const formDestination = this.destinationsModel.getById(this.tripPoints[0].id);
-    const formOffers = this.offersModel.getByType(this.tripPoints[0].type);
+    const formDestination = this.#destinationsModel.getById(this.#tripPoints[0].id);
+    const formOffers = this.#offersModel.getByType(this.#tripPoints[0].type);
 
     render(new TripInfoView(), tripMainElement, RenderPosition.AFTERBEGIN);
     render(new FilterView(), tripControlsElement);
     render(new SortView(), tripEventsElement);
-    render(this.contentComponent, tripEventsElement);
+    render(this.#contentComponent, tripEventsElement);
 
-    render(this.formComponent, this.contentComponent.element);
+    render(this.#formComponent, this.#contentComponent.element);
     render(new FormHeaderView({
-      tripPoint: this.tripPoints[0],
-      destinationList: this.destinations,
+      tripPoint: this.#tripPoints[0],
+      destinationList: this.#destinations,
       destination: formDestination
     }), formElement);
 
-    render(this.formDetailsComponent, formElement);
+    render(this.#formDetailsComponent, formElement);
     if (formDestination) {
       render(new FormDestinationView({destination: formDestination}),
-        this.formDetailsComponent.element);
+        this.#formDetailsComponent.element);
     }
 
     if (formOffers.length) {
       render(new FormOffersView({
-        pointOffers: this.tripPoints[0].offers,
+        pointOffers: this.#tripPoints[0].offers,
         offers: formOffers
-      }), this.formDetailsComponent.element);
+      }), this.#formDetailsComponent.element);
     }
 
-    for (let i = 0; i < this.tripPoints.length; i++) {
-      const offers = this.offersModel.getByType(this.tripPoints[i].type);
-      const destination = this.destinationsModel.getById(this.tripPoints[i].id);
+    for (let i = 0; i < this.#tripPoints.length; i++) {
+      const offers = this.#offersModel.getByType(this.#tripPoints[i].type);
+      const destination = this.#destinationsModel.getById(this.#tripPoints[i].id);
 
       render(new TripPointView({
-        tripPoint: this.tripPoints[i],
+        tripPoint: this.#tripPoints[i],
         offers: offers,
         destination: destination
-      }), this.contentComponent.element);
+      }), this.#contentComponent.element);
     }
   }
 }
