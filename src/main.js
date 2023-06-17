779cdb38d8eda29f9d2626af9f1ac36f4c6c057d
margo-chek <mnchek@mail.ru>
2023-06-17 20:47:48 +0300

    Добавлена модель для фильтра

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import MainPresenter from './presenter/main-presenter.js';
 import TripPointsModel from './model/trip-points-model.js';
 import OffersModel from './model/offers-model.js';
 import DestinationsModel from './model/destinations-model.js';
+import FilterModel from './model/filter-model.js';
 
 const tripPointsModel = new TripPointsModel();
 const offersModel = new OffersModel();
 const destinationsModel = new DestinationsModel();
+const filterModel = new FilterModel();
 
 const mainPresenter = new MainPresenter({
   pointsModel: tripPointsModel,
